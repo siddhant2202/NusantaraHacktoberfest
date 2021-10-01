680d3c34709f0db45e6adc9ac3f3012ebc92f748
Ayra Hikari <AyraHikari@linuxmail.org>
2021-10-01 13:25:33 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Happy Hacktoberfest!
+What it is?
+> This is just simple script that print your name and text in python language
+
+# How to pull request?
+- Fork this repo
+- Edit `hacktoberfest.py`
+- Insert `me("your name", "whatever text")` in last of `def contributors():`
+- Example:
+```
+def contributors():
+	me("Ayra", "Feel free to pull request!")
+
+	me("your name", "whatever text")
+```
+- Go to main repository, then create a pull request
---
+++ hacktoberfest.py
@@ -0,0 +1,23 @@
+def me(name, about):
+	print("- {}: {}".format(name, about))
+
+def contributors():
+	# How to add your name:
+	#  insert same like below, in last of def contributors():
+	#    me("your name", "your wish")
+	#
+	# Example:
+	#
+	# def contributors():
+	#    me("first contributor", "happy hacktoberfest")
+	#    me("your contributor", "i love cat~")  <-- your example pull request
+	# 
+	# More info check pull requests
+	# 
+	me("Ayra", "Feel free to pull request!")
+
+
+print(" ")
+print("Happy Hacktoberfest 2021!")
+print("Here is our contributors from worldwide:")
+contributors()
