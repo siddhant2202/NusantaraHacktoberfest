c0778981fb79826bdb82692755b9c19efb71e28e
Mubashir Rehman <mubashirrehman66@gmail.com>
2021-10-04 10:52:55 +0500

    Added Mubashir Rehman
    Thanks for the PR example.

--- hacktoberfest.py
+++ hacktoberfest.py
@@ -30,6 +30,7 @@ def contributors():
     me("Najahi", "Yoo!")
     me("Shreyas", "Hello Hacktober2021")
     me("DHARSHINI", "PYTHON IS LOVE, Thanks for giving me a beginner level challenge, i am a beginner tho, learning pull request")
+    me("Mubashir Rehman", "Python is comparably easier to understand as compared to c++.")
 
 
 print(" ")
