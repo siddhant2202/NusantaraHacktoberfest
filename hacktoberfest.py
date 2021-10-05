1d67e8f99f6da76dcecc0be5de3ad2dbb2853dae
AjitVarpe <52324866+AjitVarpe@users.noreply.github.com>
2021-10-05 12:12:33 +0530

    Update hacktoberfest.py

--- hacktoberfest.py
+++ hacktoberfest.py
@@ -30,6 +30,7 @@ def contributors():
     me("Najahi", "Yoo!")
     me("Shreyas", "Hello Hacktober2021")
     me("DHARSHINI", "PYTHON IS LOVE, Thanks for giving me a beginner level challenge, i am a beginner tho, learning pull request")
+    me("Ajit", "Hello Hacktober2021")
 
 
 print(" ")
